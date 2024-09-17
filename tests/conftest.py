3f632013041296e025208655ba0df905e4bdb665
ddiop <ddiop.ext@talentia-software.com>
2024-09-17 20:50:12 +0200

    add new repo

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,12 @@
 import pytest
 from unittest.mock import MagicMock, patch
+from database.postgresql_functools import PostgresManager
 
 
 @pytest.fixture
 def mock_postgres_manager():
     """Fixture pour mocker PostgresManager."""
-    with patch('postgresql_functools.PostgresManager') as MockPostgresManager:
+    with patch('database.postgresql_functools.PostgresManager') as MockPostgresManager:
         mock_instance = MockPostgresManager.return_value
 
         # Mock des méthodes pour simuler l'ajout, la récupération et la suppression des données
